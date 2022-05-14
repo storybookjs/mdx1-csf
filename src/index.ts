fa6f535ce8c9e31fc079f9cf925c4015fedf4e25
joshwooding <12938082+joshwooding@users.noreply.github.com>
2022-05-14 19:56:21 +0100

    Support skipCsf option in compile functions

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import mdx from '@mdx-js/mdx';
 import { createCompiler, MdxOptions } from './sb-mdx-plugin';
 
 export const compile = async (code: string, options?: MdxOptions) =>
-  mdx(code, { compilers: [createCompiler(options)] });
+  mdx(code, { compilers: options.skipCsf ? undefined : [createCompiler(options)] });
 
 export const compileSync = (code: string, options?: MdxOptions) =>
-  mdx.sync(code, { compilers: [createCompiler(options)] });
+  mdx.sync(code, { compilers: options.skipCsf ? undefined : [createCompiler(options)] });
 
 export * from './sb-mdx-plugin';
